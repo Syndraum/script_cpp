d37e1bd1b5bf56a51f0b47354ce214cb0d672273
Alvarez Romain <roalvare@student.42.fr>
2021-01-05 19:16:04 +0100

    no argument for update

--- update.py
+++ update.py
@@ -1,13 +1,18 @@
 import sys
 from Cpp import Cpp
+from os import listdir
+from os.path import isfile, join
 
 def main(argv):
 	if (len(argv) == 0):
-		print("Need name of one class")
-		return 1
-	for arg in argv:
-		Myclass = Cpp(arg)
-		Myclass.update()
+		files = [f[:-4] for f in listdir("./") if isfile(join("./", f)) and f[-4:] == ".cpp"]
+		for f in files:
+			Myclass = Cpp(f)
+			Myclass.update()
+	else:
+		for arg in argv:
+			Myclass = Cpp(arg)
+			Myclass.update()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])

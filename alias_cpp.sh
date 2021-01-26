3e68076d548a903cae800c389856ccf12dedafad
Syndraum <roms2111@gmail.com>
2021-01-26 15:17:53 +0100

    change alias

--- alias_cpp.sh
+++ alias_cpp.sh
@@ -1,4 +1,7 @@
-DIRCPPSCRIPT=$(dirname "$0") > /dev/null
+#!/bin/bash
 
+DIRCPPSCRIPT=$(dirname -- "$BASH_SOURCE") > /dev/null
+
+alias cppclang="clang++ -Werror -Wextra -Wall -std=c++98 *.cpp"
 alias cppclass="python3 $DIRCPPSCRIPT/class.py"
 alias cppupdate="python3 $DIRCPPSCRIPT/update.py"

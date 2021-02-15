428e3e1ad7648ba173cc8bd95e1d34c5a0b13511
Alvarez Romain <roalvare@student.42.fr>
2021-02-15 22:22:33 +0100

    chnage alias

--- alias_cpp.sh
+++ alias_cpp.sh
@@ -6,6 +6,6 @@ else
 	DIRCPPSCRIPT=$(dirname -- "$0") > /dev/null
 fi
 
-alias cppclang="clang++ -Werror -Wextra -Wall -std=c++98 *.cpp"
+alias clangall="clang++ -Werror -Wextra -Wall -std=c++98 *.cpp"
 alias cppclass="python3 $DIRCPPSCRIPT/class.py"
 alias cppupdate="python3 $DIRCPPSCRIPT/update.py"
